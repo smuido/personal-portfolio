8939171d8b244e8bffd6200036823639726241ba
Smaya Guido <23smayaguido@gmail.com>
2026-07-18 10:25:21 -0700

    fix config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/personal-portfolio/',
+  base: '/cp-portfolio/',
   plugins: [react(), tailwindcss()],
 })
